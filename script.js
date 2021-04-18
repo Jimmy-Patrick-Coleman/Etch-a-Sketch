94c41e20bd377e8c52a9859cc053f1f2c071cc8e
Spoon <JimmyPColeman@outlook.com>
2021-04-18 14:32:52 +0100

    made it a 16 x 16 grid

--- script.js
+++ script.js
@@ -1,65 +1,7 @@
-const container = document.querySelector('#container');
+const gridContainer = document.querySelector('#grid-container');
 
-const divOne = document.createElement('div');
-divOne.classList.add('divOne');
-container.appendChild(divOne);
-
-const divTwo = document.createElement('div');
-divTwo.classList.add('divTwo');
-container.appendChild(divTwo);
-
-const divThree = document.createElement('div');
-divThree.classList.add('divThree');
-container.appendChild(divThree);
-
-const divFour = document.createElement('div');
-divFour.classList.add('divFour');
-container.appendChild(divFour);
-
-const divFive = document.createElement('div');
-divFive.classList.add('divFive');
-container.appendChild(divFive);
-
-const divSix = document.createElement('div');
-divSix.classList.add('divSix');
-container.appendChild(divSix);
-
-const divSeven = document.createElement('div');
-divSeven.classList.add('divSeven');
-container.appendChild(divSeven);
-
-const divEight = document.createElement('div');
-divEight.classList.add('divEight');
-container.appendChild(divEight);
-
-const divNine = document.createElement('div');
-divNine.classList.add('divNine');
-container.appendChild(divNine);
-
-const divTen = document.createElement('div');
-divTen.classList.add('divTen');
-container.appendChild(divTen);
-
-const divEleven = document.createElement('div');
-divEleven.classList.add('divEleven');
-container.appendChild(divEleven);
-
-const divTwelve = document.createElement('div');
-divTwelve.classList.add('divTwelve');
-container.appendChild(divTwelve);
-
-const divThirteen = document.createElement('div');
-divThirteen.classList.add('divThirteen');
-container.appendChild(divThirteen);
-
-const divFourteen = document.createElement('div');
-divFourteen.classList.add('divFourteen');
-container.appendChild(divFourteen);
-
-const divFifteen = document.createElement('div');
-divFifteen.classList.add('divFifteen');
-container.appendChild(divFifteen);
-
-const divSixteen = document.createElement('div');
-divSixteen.classList.add('divSixteen');
-container.appendChild(divSixteen);
+for (let i = 0; i < 256; i++) {
+const div = document.createElement('div');
+div.classList.add('div');
+gridContainer.appendChild(div);
+}
