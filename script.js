bf0be9ffd77d2a8372b919c0c01aa078e664e317
Spoon <Jimmy-Patrick-Coleman@outlook.com>
2021-04-22 10:26:34 +0100

    restarting agian

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const gridContainer = document.querySelector('#grid-container');
+const buttonContainer = document.querySelector('#button-container')
+const blueBtn = document.createElement('button');
 
+const gridContainer = document.querySelector('#grid-container');
 function createDivs(size) {
-    for(let i = 0; i < size; i++) {
-        for(let j = 0; j < size; j++) {
-            const div = document.createElement('div');
-            div.classList.add('div');  
-            gridContainer.appendChild(div);
-        }
+    for(let i = 0; i < (size * size); i++) {
         const div = document.createElement('div');
-        div.classList.add('div');  
-        gridContainer.appendChild(div);
+        gridContainer.appendChild(div).classList.add('div'); ;
     }
    
 }
 createDivs(10);
+
+function makeBlue() {
+const divs = gridContainer.querySelectorAll('.div');
+blueBtn.textContent = 'blue';
+blueBtn.addEventListener('click', () => {
+  divs.forEach(div => div.addEventListener('mouseover', () => {
+    div.setAttribute('style', 'color: blue; background: white'); 
+}))
+
+})
+buttonContainer.appendChild(blueBtn);
+}
+makeBlue();
