e808c4dc140913e1ca01944053a9d2c8da655018
Spoon <Jimmy-Patrick-Coleman@outlook.com>
2021-04-22 09:55:14 +0100

    function adds working divs

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const gridContainer = document.querySelector('#grid-container');
 
 function createDivs(size) {
     for(let i = 0; i < size; i++) {
-        const column = document.createElement('div');
-        column.classList.add('column');
-        column.setAttribute('style', 'border: 1px solid red;');
-        gridContainer.appendChild(column);
+        for(let j = 0; j < size; j++) {
+            const div = document.createElement('div');
+            div.classList.add('div');  
+            gridContainer.appendChild(div);
+        }
+        const div = document.createElement('div');
+        div.classList.add('div');  
+        gridContainer.appendChild(div);
     }
-    for(let j = 0; j < size; j++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        row.setAttribute('style', 'border: 1px solid blue;');    
-        gridContainer.appendChild(row);  
-    }
+   
 }
 createDivs(10);
